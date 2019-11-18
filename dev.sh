3a68cce4f63e747446c14bb97a52d0b746b2faa9
Bryton Hall <email@bryton.io>
2019-11-18 12:17:28 -0500

    move chart into subdirectory

--- dev.sh
+++ dev.sh
@@ -4,7 +4,7 @@ k3d create
 sleep 5 # wait for kubeconfig to become available
 export KUBECONFIG=$(k3d get-kubeconfig)
 
-helm install hf . --namespace hobbyfarm -f values-ignored.yaml --wait
+helm install hf charts/hobbyfarm --namespace hobbyfarm --values values-ignored.yaml --wait
 
 # this can't be executed until all pods attached to services are running
 sudo -E kubefwd services -n hobbyfarm
