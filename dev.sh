2a895e4678ce26b52c58a3986d1b399b49cc9dc8
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2020-01-06 10:36:58 -0500

    Update dev.sh for namespace and new path (#7)
    * Update dev.sh for namespace and new path
    
    * Make sure script is executed in the correct dir

--- dev.sh
+++ dev.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -e
 
+cd $(dirname $0)
+
 k3d create
-sleep 5 # wait for kubeconfig to become available
+sleep 7 # wait for kubeconfig to become available
 export KUBECONFIG=$(k3d get-kubeconfig)
 
-helm install hf charts/hobbyfarm --namespace hobbyfarm --values values-ignored.yaml --wait
+kubectl create ns hobbyfarm
+
+helm install hf charts/hobbyfarm --namespace hobbyfarm --values charts/hobbyfarm/values.yaml --wait
 
 # this can't be executed until all pods attached to services are running
 sudo -E kubefwd services -n hobbyfarm
